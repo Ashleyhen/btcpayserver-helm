35746a45503db627df9414ebbd8ae3e6c17036bc
ashleyhen <hendrash@mail.gvsu.edu>
2023-01-24 13:42:01 -0500

    btc_pay_server

--- index.ts
+++
@@ -1,9 +0,0 @@
-import * as k8s from "@pulumi/kubernetes";
-import {deployment} from "./deployments/bitcoind";
-import {lightningdDeployment} from "./deployments/lightningd"
-import {btcServerDeployment} from "./deployments/btcpay-server"
-// bitcoin set up
-
-export const bitcoind =deployment.metadata.name;
-export const lightningd=lightningdDeployment.metadata.name
-export const btcpayserver = btcServerDeployment.metadata.name;
