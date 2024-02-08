1f181cdfae9f77bad6dbe2c6a15db3c32f818e75
ashleyhen <Ashleyhen>
2024-02-08 17:16:12 -0500

    fix: remove old charts

--- chart/nbxplorer/up.sh
+++
@@ -1,5 +0,0 @@
-    helm upgrade -i nbxplorer . -n playpen --create-namespace \
-    --set rabbitmq.auth.password='test123' \
-    --set rabbitmq.auth.erlangCookie='test123' \
-    --set config.rpcauth='lsdopenlnd:PItVvAqMJl_OmpQc54jVIUpCMM9rLEmhoF2h4GHEH8E='
-    $@
